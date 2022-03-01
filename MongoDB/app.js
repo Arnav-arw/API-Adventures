8bfb6bdc33e8ea719f9d163c528dc0ff9655a8b1
Arnav Singhal <singhal.arnav@gmail.com>
2022-03-01 14:06:48 +0530

    Connected to database!

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -2,16 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+require('dotenv/config');
+
+// Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/post', postsRoute);
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
     });
 
-app.get('/post', (req, res) => {
-    res.send('Hello Post!');
-    });
-
 // Connect to database
-
+mongoose.connect(process.env.DB_CONNECTION, () =>
+    console.log('Connected to database!')
+    );
 
 app.listen(3000);
