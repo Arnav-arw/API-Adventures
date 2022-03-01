b59df0bc8a8d5f6dee93fc8f633bf69c1ad546a2
Arnav Singhal <singhal.arnav@gmail.com>
2022-03-01 14:53:48 +0530

    Added fetch all posts

--- MongoDB/Routes/posts.js
+++ MongoDB/Routes/posts.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('Hello Posts!');
-    });
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (error) {
+        res.json({ message: error });
+    }
+});
 
 router.post('/', async (req, res) => {
     const post = new Post({
